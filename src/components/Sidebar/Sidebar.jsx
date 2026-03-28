3ff06870f48b32ee75c2f40b56dcdec3fb7e3d83
Vickymaurya1 <vickymaurya2005@gmail.com>
2026-03-28 20:15:40 +0530

    Updated the Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import './Sidebar.css'
-import {assets} from '../../assets/assets'
+import './Sidebar.css';
+import {assets} from '../../assets/assets';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className="top">
-        <img src= {assets.menu_icon} alt="" />
-         <div className="newchat">
+        <img className="menu" src={assets.menu_icon} alt="Menu" />
+         <div className="new-chat">
             <img src={assets.plus_icon} alt="" />
             <p>New Chat</p>
          </div>
          <div className="recent">
-            <p className='recent-title'>
-                Recent
-                <div className="recent-entry">
-                    <img src={assets.message_icon} alt="" />
-                    <p>What is react...</p>
-                </div>
-            </p>
+            <p className='recent-title'>Recent</p>
+            <div className="recent-entry">
+                <img src={assets.message_icon} alt="" />
+                <p>What is react...</p>
+            </div>
          </div>
       </div>
       <div className="bottom">
